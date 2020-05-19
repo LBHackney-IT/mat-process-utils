64fbd626a5d61f1b6d59ab2f4b00282ac250a7ab
F <f@dxw.com>
2020-05-19 16:40:53 +0100

    Upgrade and improve rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,75 @@
 /* eslint-env node */
-const { dirname } = require("path");
-const babel = require("rollup-plugin-babel");
-const del = require("rollup-plugin-delete");
-const { default: multiInput } = require("rollup-plugin-multi-input");
-const resolve = require("rollup-plugin-node-resolve");
-const progress = require("rollup-plugin-progress");
-const typescript = require("rollup-plugin-typescript2");
+import { DEFAULT_EXTENSIONS } from "@babel/core";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import { dirname } from "path";
+import del from "rollup-plugin-delete";
+import multiInput from "rollup-plugin-multi-input";
+import progress from "rollup-plugin-progress";
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
-const pkg = require("./package.json");
-const tsconfig = require("./tsconfig.json");
-
-module.exports = [
-  {
-    input: ["src/**/*.ts?(x)", "!**/__*__/**/*", "!**/*.(spec|test).*"],
-    output: [
-      {
-        dir: dirname(pkg.main),
-        format: "cjs",
-      },
-      {
-        dir: dirname(pkg.module),
-        format: "es",
-      },
-    ],
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ],
-    plugins: [
-      multiInput(),
-      del({
-        targets: [dirname(pkg.main), dirname(pkg.module)],
-        verbose: true,
-      }),
-      progress({
-        clearLine: false,
-      }),
-      resolve(),
-      typescript({
-        typescript: require("typescript"),
-        useTsconfigDeclarationDir: true,
-        tsconfigOverride: {
-          compilerOptions: {
-            declarationDir: dirname(pkg.types),
-          },
-          exclude: [
-            ...tsconfig.exclude,
-            "**/__*__/**/*",
-            "**/*.spec.*",
-            "**/*.test.*",
-          ],
-        },
-      }),
-      babel({
-        extensions: [".ts", ".tsx"],
-        exclude: "**/node_modules/**/*",
-      }),
-    ],
-  },
+const dependencies = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
 ];
+
+let hadCriticalWarning = false;
+
+module.exports = {
+  input: ["src/**/*.ts?(x)", "!**/__*__/**/*", "!**/*.(spec|test).*"],
+  output: [
+    {
+      dir: dirname(pkg.main),
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      dir: dirname(pkg.module),
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  external: (id) =>
+    dependencies.some((dependency) =>
+      new RegExp(`^(.+/)?${dependency}(/.+)?$`).test(id)
+    ),
+  plugins: [
+    multiInput(),
+    del({
+      targets: [dirname(pkg.main), dirname(pkg.module)],
+      verbose: true,
+    }),
+    progress({
+      clearLine: false,
+    }),
+    resolve({
+      preferBuiltins: false,
+    }),
+    commonjs(),
+    typescript({
+      tsconfigOverride: {
+        exclude: ["**/__*__/**/*", "**/*.spec.*", "**/*.test.*"],
+      },
+    }),
+    babel({
+      babelHelpers: "inline",
+      extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"],
+    }),
+    {
+      generateBundle() {
+        if (hadCriticalWarning) {
+          throw new Error("A critical warning occurred");
+        }
+      },
+    },
+  ],
+  onwarn(warning, onwarn) {
+    onwarn(warning, onwarn);
+
+    if (!warning.message.startsWith("Generated an empty chunk: ")) {
+      hadCriticalWarning = true;
+    }
+  },
+};

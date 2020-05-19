023ed1aa5623f24d308305c8b11b1f89b8e1c308
F <f@dxw.com>
2020-05-19 16:40:53 +0100

    Upgrade and improve rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,82 @@
 /* eslint-env node */
-const { dirname } = require("path");
-const babel = require("rollup-plugin-babel");
-const del = require("rollup-plugin-delete");
-const { default: multiInput } = require("rollup-plugin-multi-input");
-const resolve = require("rollup-plugin-node-resolve");
-const progress = require("rollup-plugin-progress");
-const typescript = require("rollup-plugin-typescript2");
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { dirname } from "path";
+import del from "rollup-plugin-delete";
+import multiInput from "rollup-plugin-multi-input";
+import progress from "rollup-plugin-progress";
+import pkg from "./package.json";
 
-const pkg = require("./package.json");
-const tsconfig = require("./tsconfig.json");
+const dependencies = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+let hadCriticalWarning = false;
+
+const makeConfig = ({ outputDir, outputFormat, typescriptConfig }) => ({
+  input: ["src/**/*.ts?(x)", "!**/__*__/**/*", "!**/*.(spec|test).*"],
+  output: [
+    {
+      dir: outputDir,
+      format: outputFormat,
+      sourcemap: true,
+    },
+  ],
+  external: (id) =>
+    dependencies.some((dependency) =>
+      new RegExp(`^(.+/)?${dependency}(/.+)?$`).test(id)
+    ),
+  plugins: [
+    multiInput(),
+    del({
+      targets: outputDir,
+      verbose: true,
+    }),
+    progress({
+      clearLine: false,
+    }),
+    resolve({
+      preferBuiltins: false,
+    }),
+    commonjs(),
+    typescript(typescriptConfig),
+    babel({
+      babelHelpers: "inline",
+      extensions: [".ts", ".tsx"],
+    }),
+    {
+      generateBundle() {
+        if (hadCriticalWarning) {
+          throw new Error("A critical warning occurred");
+        }
+      },
+    },
+  ],
+  onwarn(warning, onwarn) {
+    onwarn(warning, onwarn);
+
+    if (!warning.message.startsWith("Generated an empty chunk: ")) {
+      hadCriticalWarning = true;
+    }
+  },
+});
 
 module.exports = [
-  {
-    input: ["src/**/*.ts?(x)", "!**/__*__/**/*", "!**/*.(spec|test).*"],
-    output: [
-      {
-        dir: dirname(pkg.main),
-        format: "cjs",
-      },
-      {
-        dir: dirname(pkg.module),
-        format: "es",
-      },
-    ],
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ],
-    plugins: [
-      multiInput(),
-      del({
-        targets: [dirname(pkg.main), dirname(pkg.module)],
-        verbose: true,
-      }),
-      progress({
-        clearLine: false,
-      }),
-      resolve(),
-      typescript({
-        typescript: require("typescript"),
-        useTsconfigDeclarationDir: true,
-        tsconfigOverride: {
-          compilerOptions: {
-            declarationDir: dirname(pkg.types),
-          },
-          exclude: [
-            ...tsconfig.exclude,
-            "**/__*__/**/*",
-            "**/*.spec.*",
-            "**/*.test.*",
-          ],
-        },
-      }),
-      babel({
-        extensions: [".ts", ".tsx"],
-        exclude: "**/node_modules/**/*",
-      }),
-    ],
-  },
+  makeConfig({
+    outputDir: dirname(pkg.module),
+    outputFormat: "es",
+    typescriptConfig: {
+      declarationDir: dirname(pkg.types),
+      declaration: true,
+      declarationMap: true,
+    },
+  }),
+  makeConfig({
+    outputDir: dirname(pkg.main),
+    outputFormat: "cjs",
+    typescriptConfig: {},
+  }),
 ];

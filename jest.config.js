1893123238aabbad6c9ef301aff3eccbbe4251e4
F <f@dxw.com>
2020-05-19 16:43:52 +0100

    Add in some known dependencies

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,11 @@ module.exports = {
   snapshotSerializers: ["<rootDir>/src/__tests__/serializers/pathSerializer"],
   restoreMocks: true,
   testMatch: ["**/*.+(spec|test).ts?(x)"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/examples/"],
+  testPathIgnorePatterns: ["node_modules/", "<rootDir>/examples/"],
   globals: {
     "ts-jest": {
       tsConfig: "<rootDir>/tsconfig.json",
+      babelConfig: "<rootDir>/babel.config.js",
     },
   },
 };
